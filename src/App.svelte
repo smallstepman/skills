bc43140f261f9bb0a8fd392d46af3f5df3bf3a12
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2021-12-07 06:53:53 +0100

    first version

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,61 @@
-<script>
-	export let name;
+<script lang="ts">
+	import ForceGraph3D from '3d-force-graph';
+	import SpriteText from 'three-spritetext';
+	import { onMount } from "svelte";
+
+	let canvasDom;//: HTMLElement;
+
+	onMount(() => {
+		fetch('./dataset.json').then(res => res.json()).then(data => {
+			const Graph = ForceGraph3D()(canvasDom)
+				.graphData(data)
+				.nodeId('id')
+				.nodeVal('val')
+				.nodeLabel('id')
+				.nodeAutoColorBy('group')
+				.linkSource('source')
+				.linkTarget('target')
+				.linkColor('rgba(255, 0, 0, 0.8)')
+				.nodeThreeObject(node => {
+					const sprite = new SpriteText(node.id);
+					sprite.material.depthWrite = false; // make sprite background transparent
+					sprite.color = node.color;
+					sprite.textHeight = 8;
+					return sprite;
+				})
+			// .nodeCanvasObject((node, ctx, globalScale) => {
+			// 					const label = node.id;
+			// 					const fontSize = 18/globalScale;
+			// 					ctx.font = `${fontSize}px Sans-Serif`;
+			// 					const textWidth = ctx.measureText(label).width;
+			// 					const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+			//
+			// 					ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+			// 					ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+			//
+			// 					ctx.textAlign = 'center';
+			// 					ctx.textBaseline = 'middle';
+			// 					ctx.fillStyle = 'rgba(255, 0, 0, 0.8)';
+			// 					ctx.fillText(label, node.x, node.y);
+			//
+			// 					node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+			// 				})
+		Graph.d3Force('charge').strength(-120)
+		})
+	});
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div bind:this={canvasDom} />
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
 	@media (min-width: 640px) {
 		main {
